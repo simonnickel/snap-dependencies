93e4969685065c5297d06d169014396e1abed498
Simon Nickel <simonnickel@googlemail.com>
2024-10-26 21:42:01 +0200

    Fix deadlock by locking the instance insertion

--- Sources/SnapDependencies/Dependencies+Container.swift
+++ Sources/SnapDependencies/Dependencies+Container.swift
@@ -16,7 +16,7 @@ internal extension Dependencies {
 		
 		/// **Thread Safety**: Access has to be guarded by a queue.
 		private var instances: [String: Any] = [:]
-
+		
 		
 		// MARK: - Register
 		
@@ -47,23 +47,33 @@ internal extension Dependencies {
 			}
 		}
 		
+		/// A lock to prevent creating multiple instances
+		private var lockCreation = os_unfair_lock_s()
+		
 		// TODO: Define Key type
 		// TODO: Generate key for type
 		private func create<Dependency>(type: Dependency.Type, in queue: DispatchQueue) -> Dependency? {
 			let key: String = "\(type)"
 			
-			/// **Thread Safety**: Creating is serial to prevent data races.
-			return queue.sync(flags: .barrier) {
-				// Need to check again, because it could be created during concurrent resolve, while waiting for .barrier.
-				if let resolved = instances[key] as? Dependency {
-					return resolved
-				}
+			/// **Thread Safety**: Using `queue.sync(flags: .barrier)` causes a deadlock when the resolved dependency has to resolve another dependency during it's initialisation.
+			// To prevent data races:
+			// * inserting the instance is serialised by a lock
+			// * existing instances are checked again before inserting
+			return queue.sync {
 				guard let factory = dependencies[key] else { return nil }
+				// Creating can not be secured by lock, would deadlock when the init has to create a dependency.
 				guard let resolved = factory() as? Dependency else { return nil }
 				
-				Logger.dependencies.debug("Created `\(key)` from factory")
+				os_unfair_lock_lock(&lockCreation)
+				// Need to check again, because it could be created during concurrent resolve, while waiting for .barrier.
+				if (instances[key] as? Dependency) == nil {
+					Logger.dependencies.debug("Created `\(key)` from factory")
+					instances[key] = resolved
+				} else {
+					return nil
+				}
+				os_unfair_lock_unlock(&lockCreation)
 				
-				instances[key] = resolved
 				return resolved
 			}
 		}
